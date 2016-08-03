90caaee7c86ba184eacf9c2904affdab24b9e958
Ian Schoonover <iantskon@gmail.com>
2016-08-03 09:26:45 -0500

    Add category and brand filters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :categories, :brands
+
+  def categories
+  	@categories = Category.all
+  end
+
+  def brands
+  	@brands = Product.pluck(:brand).sort.uniq!
+  	if @brands.nil?
+  		@brands = Product.pluck(:brand).sort
+  	end
+  end
 end
